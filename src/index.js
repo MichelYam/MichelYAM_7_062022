48ade2c6a7869752c1cab308627219a7545ee564
MichelYAM <michel.yam92@gmail.com>
2022-06-16 19:09:58 +0200

    fix filted advanced

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
 import recipes from '../data/recipes.js';
 import getAllRecipes from './recipes.js';
@@ -6,6 +7,8 @@ import {
     appliancesTags,
     ustensilsTags,
     tagsList,
+    filterList,
+    test,
 } from './tags.js';
 
 const searchInput = document.getElementById('search');
@@ -13,6 +16,12 @@ const ingredientsList = document.querySelector('#blue-content ul');
 const appliancesList = document.querySelector('#green-content ul');
 const ustensilsList = document.querySelector('#red-content ul');
 
+const ingredientInput = document.getElementById('ingredients-input');
+const deviceInput = document.getElementById('devices-input');
+const ustesilsInput = document.getElementById('ustensiles-input');
+
+let tagsArray = [];
+let filterTags = recipes;
 function toggleItem() {
     const filterBtn = document.querySelectorAll('.filter-btn');
     const itemClass = this.parentNode.parentNode.parentNode.className;
@@ -58,6 +67,7 @@ function handleRecipe() {
                 .includes(inputValue))) {
                 return true;
             }
+            return false;
         });
         return recipeArr;
     }
@@ -65,19 +75,137 @@ function handleRecipe() {
 }
 
 function displayTagsList() {
-    ingredientsList.innerHTML = tagsList(ingrediantsTags(recipes));
-    appliancesList.innerHTML = tagsList(appliancesTags(recipes));
-    ustensilsList.innerHTML = tagsList(ustensilsTags(recipes));
+    ingredientsList.innerHTML = filterList(ingrediantsTags(recipes));
+    appliancesList.innerHTML = filterList(appliancesTags(recipes));
+    ustensilsList.innerHTML = filterList(ustensilsTags(recipes));
 }
 
 searchInput.addEventListener('input', () => {
     displayRecipes(handleRecipe());
 });
 
+ingredientInput.addEventListener('input', () => {
+    ingredientsList.innerHTML = filterList(test(ingredientInput, ingrediantsTags(recipes)));
+});
+deviceInput.addEventListener('input', () => {
+    appliancesList.innerHTML = filterList(test(deviceInput, appliancesTags(recipes)));
+});
+ustesilsInput.addEventListener('input', () => {
+    ustensilsList.innerHTML = filterList(test(ustesilsInput, ustensilsTags(recipes)));
+});
+
+function renderTags(tags) {
+    const tagsSection = document.querySelector('.tags__list');
+    tagsSection.innerHTML = tagsList(tags);
+
+    const allTags = document.querySelectorAll('.tags__card svg');
+    allTags.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            removeTag(e);
+        });
+    });
+}
+function removeTag(e) {
+    // Supprime le tag lors du click
+    tagsArray = tagsArray.filter(
+        (tag) => tag.name !== e.target.parentNode.textContent,
+    );
+
+    filterTags = recipes;
+    // check s'il y a au moins 1 tag
+    if (tagsArray.length > 0) {
+        filterRecipeByTag();
+    }
+    // maj des items dans les filtres
+    updateFilterListData(filterTags);
+
+    // Affichage des recettes filtrées
+    displayRecipes(filterTags);
+
+    // affiche les tags dans les filtres respectifs
+    renderTags(tagsArray);
+    // filterTags = recipes;
+}
+
+function addTag(e) {
+    // creation d'objet
+    const newTag = {
+        name: e.target.textContent,
+        type: e.target.offsetParent.classList[1],
+    };
+
+    // Verifie si l'objet existe dans le tableau
+    const id = tagsArray.findIndex((item) => item.name === newTag.name);
+
+    if (id < 0) {
+        // ajoute dans le tableau
+        tagsArray.push(newTag);
+        filterRecipeByTag();
+        // console.log(filterTags);
+        // Affiche les recettes en fonction des tags ajoutés
+        displayRecipes(filterTags);
+        // Mets a jour les filtres
+        updateFilterListData(filterTags);
+        // affichage du tag
+        renderTags(tagsArray);
+    }
+
+    // console.log(tagsArray);
+}
+
+// recuperer toutes les recettes  en fonction de l'ingredient dans les tags
+function filterRecipeByTag() {
+    tagsArray.forEach((tag) => {
+        switch (tag.type) {
+            case 'ingredients':
+                filterTags = filterTags.filter((recipe) =>
+                    recipe.ingredients.some((ingredient) =>
+                        ingredient.ingredient.toLowerCase().includes(tag.name.toLowerCase())));
+                break;
+            case 'devices':
+                filterTags = filterTags.filter((recipe) => recipe.appliance.toLowerCase()
+                    .includes(tag.name.toLowerCase()),
+                );
+                break;
+            case 'utensils':
+                filterTags = filterTags.filter((recipe) =>
+                    recipe.ustensils.some((ustensil) =>
+                        ustensil.toLowerCase().includes(tag.name.toLowerCase())));
+                break;
+            default:
+                console.log('failed');
+        }
+    });
+    // console.log(newRecipes);
+    return filterTags;
+}
+
+/**
+ * affiche du nouveau filtre ingredient
+ * @param {*} arr ;
+ */
+function updateFilterListData() {
+    ingredientsList.innerHTML = filterList(ingrediantsTags(filterTags));
+    appliancesList.innerHTML = filterList(appliancesTags(filterTags));
+    ustensilsList.innerHTML = filterList(ustensilsTags(filterTags));
+    handleTagsChecked();
+}
+
+function handleTagsChecked() {
+    // recuperes tous les li
+    const allItems = document.querySelectorAll('.dropdown-item');
+    allItems.forEach((elem) => {
+        elem.addEventListener('click', (e) => {
+            addTag(e);
+        });
+    });
+}
+
 function init() {
     handleDownMenu();
     displayTagsList();
     displayRecipes(recipes);
+    handleTagsChecked();
 }
 
 init();
--- src/tags.js
+++ src/tags.js
@@ -6,8 +6,8 @@ export function ingrediantsTags(recipes) {
             ingredientsTagsList.push(element.ingredient);
         });
     });
-    ingredientsTagsList = [...new Set(ingredientsTagsList)];
     ingredientsTagsList = ingredientsTagsList.flat();
+    ingredientsTagsList = [...new Set(ingredientsTagsList)];
     // console.log(ingredientsTagsList);
     ingredientsTagsList.sort();
     return ingredientsTagsList;
@@ -37,10 +37,32 @@ export function ustensilsTags(recipes) {
     return ustensilsTagsList;
 }
 
-export function tagsList(recipes) {
+export function filterList(recipes) {
     return recipes.map((recipe) => (
-        ` <li>
-            ${recipe}
-        </li>`
+        ` <li class="dropdown-item">${recipe}</li>`
     )).join('');
 }
+
+export function tagsList(tags) {
+    return tags.map((tag) => (`
+    <div class="tags__card ${tag.type}">${tag.name}<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z" /></svg></div>
+    `)).join('');
+}
+
+export const test = (input, array) => {
+    const inputLower = input.value.toLowerCase();
+    const newArr = array.filter((item) => {
+        if (item.toLowerCase().includes(inputLower)) {
+            return true;
+        }
+        return false;
+    });
+    return newArr;
+}
+
+// export function handleTagsChecked() {
+//     const allItems = document.querySelectorAll('.dropdown-item');
+//     const allChecked = [];
+
+//     console.log(allItems);
+// }
