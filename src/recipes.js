fc64d8f0afb4b615fc8b578921a041bfafeda15e
MichelYAM <michel.yam92@gmail.com>
2022-06-28 14:51:03 +0200

    add rest of the code

--- src/recipes.js
+++ src/recipes.js
@@ -1,16 +1,28 @@
-export default function getAllRecipes(recipes) {
+/**
+ * Retourne la structure HTML de toutes les recettes passés en paramètre
+ * @param {array} recipes;
+ * @returns html
+ */
+ export default function getAllRecipes(recipes) {
+    const filterByTitle = recipes.sort((a, b) => (a.name < b.name ? -1 : 1));
+    /**
+     * Check les ingredients
+     * @param {array} item ;
+     * @returns Html structure
+     */
     function recipeIngredients(item) {
         let ingredientList = '';
         if (item.unit && item.quantity) {
-            ingredientList = `<li>${item.ingredient} ${item.quantity}${item.unit}</li>`;
+            ingredientList = `<li>${item.ingredient}: ${item.quantity}${item.unit}</li>`;
         } else if (item.quantity && !item.unit) {
-            ingredientList = `<li>${item.ingredient} ${item.quantity}</li>`;
+            ingredientList = `<li>${item.ingredient}: ${item.quantity}</li>`;
         } else {
             ingredientList = `<li>${item.ingredient}</li>`;
         }
         return ingredientList;
     }
-    return recipes.map((recipe) => (
+
+    return filterByTitle.map((recipe) => (
         ` <article class="recipe__card">
             <div class="img"></div>
             <div class="recipes__card-content">
